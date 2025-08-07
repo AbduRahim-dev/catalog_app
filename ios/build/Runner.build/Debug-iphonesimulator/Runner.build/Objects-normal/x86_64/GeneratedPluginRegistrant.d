581e93f2c723a72f4854f84b2beb56f26bc16dec
AbduRahim-dev <a.rahim@mimsoft.pk>
2025-08-07 14:51:47 +0500

    first commit

---
+++ ios/build/Runner.build/Debug-iphonesimulator/Runner.build/Objects-normal/x86_64/GeneratedPluginRegistrant.d
@@ -0,0 +1,20 @@
+dependencies: \
+  /Users/mac/Development/Flutter\ Apps/prac_app/ios/Runner/GeneratedPluginRegistrant.m \
+  /Users/mac/Development/Flutter\ Apps/prac_app/ios/Runner/GeneratedPluginRegistrant.h \
+  /Users/mac/Development/Flutter\ Apps/prac_app/build/ios/Debug-iphonesimulator/Flutter.framework/Modules/module.modulemap \
+  /Users/mac/Development/Flutter\ Apps/prac_app/build/ios/Debug-iphonesimulator/Flutter.framework/Headers/Flutter.h \
+  /Users/mac/Development/Flutter\ Apps/prac_app/build/ios/Debug-iphonesimulator/Flutter.framework/Headers/FlutterAppDelegate.h \
+  /Users/mac/Development/Flutter\ Apps/prac_app/build/ios/Debug-iphonesimulator/Flutter.framework/Headers/FlutterMacros.h \
+  /Users/mac/Development/Flutter\ Apps/prac_app/build/ios/Debug-iphonesimulator/Flutter.framework/Headers/FlutterPlugin.h \
+  /Users/mac/Development/Flutter\ Apps/prac_app/build/ios/Debug-iphonesimulator/Flutter.framework/Headers/FlutterBinaryMessenger.h \
+  /Users/mac/Development/Flutter\ Apps/prac_app/build/ios/Debug-iphonesimulator/Flutter.framework/Headers/FlutterChannels.h \
+  /Users/mac/Development/Flutter\ Apps/prac_app/build/ios/Debug-iphonesimulator/Flutter.framework/Headers/FlutterCodecs.h \
+  /Users/mac/Development/Flutter\ Apps/prac_app/build/ios/Debug-iphonesimulator/Flutter.framework/Headers/FlutterPlatformViews.h \
+  /Users/mac/Development/Flutter\ Apps/prac_app/build/ios/Debug-iphonesimulator/Flutter.framework/Headers/FlutterTexture.h \
+  /Users/mac/Development/Flutter\ Apps/prac_app/build/ios/Debug-iphonesimulator/Flutter.framework/Headers/FlutterCallbackCache.h \
+  /Users/mac/Development/Flutter\ Apps/prac_app/build/ios/Debug-iphonesimulator/Flutter.framework/Headers/FlutterDartProject.h \
+  /Users/mac/Development/Flutter\ Apps/prac_app/build/ios/Debug-iphonesimulator/Flutter.framework/Headers/FlutterEngine.h \
+  /Users/mac/Development/Flutter\ Apps/prac_app/build/ios/Debug-iphonesimulator/Flutter.framework/Headers/FlutterEngineGroup.h \
+  /Users/mac/Development/Flutter\ Apps/prac_app/build/ios/Debug-iphonesimulator/Flutter.framework/Headers/FlutterHeadlessDartRunner.h \
+  /Users/mac/Development/Flutter\ Apps/prac_app/build/ios/Debug-iphonesimulator/Flutter.framework/Headers/FlutterPluginAppLifeCycleDelegate.h \
+  /Users/mac/Development/Flutter\ Apps/prac_app/build/ios/Debug-iphonesimulator/Flutter.framework/Headers/FlutterViewController.h
---
+++ ios/build/Runner.build/Debug-iphonesimulator/Runner.build/Objects-normal/x86_64/Runner_vers.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/mac/Development/Flutter\ Apps/prac_app/ios/build/Runner.build/Debug-iphonesimulator/Runner.build/DerivedSources/Runner_vers.c
