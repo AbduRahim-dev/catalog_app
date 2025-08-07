f6c5758b334a5216f8e219a9b9a0d6dc4a333ae2
AbduRahim-dev <a.rahim@mimsoft.pk>
2025-08-07 18:46:15 +0500

    rahim updated

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -40,7 +40,7 @@ class LoginPage extends StatelessWidget {
                       style: ElevatedButton.styleFrom(
                           foregroundColor: Colors.white,
                           fixedSize: Size(100, 20)),
-                      child: Text("Log In"))
+                      child: Text("Log In Please"))
                 ],
               ),
             )
